598d420e657cfcc212377e50fce2b2d1ec3a78f3
JackeGould <jmgould913@gmail.com>
2024-01-23 21:31:18 -0500

    More pseudocode and comments added, additional JS added

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // Pseudocode
 // Add HTML and CSS for layout
-// Not much html, mostly classes for content to be added in javascript
+// Not much html, mostly classes/IDs for content to be added in javascript
 // 5 Questions: Create arrays for questions and answers
 // Global variables, need getElementById (assign a variable to an HTML element [target elements in the DOM])
 // Functions: timer, start quiz, questions, select answer, right/wrong, save score, show high score
@@ -57,8 +57,13 @@ var questions = [
         ]
     },
  ];
+
+ // In each answers array [], the text property represents the content of a possible answer to the question.
+ // The 'correct:' property is a boolean value that will be used later to create a function to let the user know whether their answer was correct/incorrect, and also record their high score.
  
  // Start the quiz with a timer set to 75. Timer left also will be the final score minus 10 sec for every wrong answer.
+ // Global variables, most of which are pulled from the html using .getElementByID
+ 
  var timeLeft = 75;
  var timerID;
  var timerEl = document.getElementById("timer");
@@ -79,3 +84,37 @@ var questions = [
 
 // JSON (JavaScript Object Notation) parse converts JSON string into a Javascript object. 
 // JSON strings consist of key-value pairs enclosed in curly brackets
+
+var shuffledQuestions, currentQuestionIndex;
+
+// Empty global variables that will be given a value in a local function
+
+// Start button - trigger the first question and next button to display, calling function startGame
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+   currentQuestionIndex++
+   setNextQuestion()
+});
+
+// Timer counts down once game is started, will show count down, if timer runs out, save current score
+function timeTick() {
+    timeLeft--;
+    timerEl.textContent = "Time: " + timeLeft;
+    if (timeLeft <= 0) {
+        saveScore();
+    }
+ }
+
+ // Start Quiz- function startGame (which we called earlier)
+function startGame() {
+    timerID = setInterval(timeTick, 1000);
+    startContainerEl.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove("hide");
+    // Timer will start as soon as start button is clicked
+    timeTick();
+    setNextQuestion();
+ };
+ 
+ // Without further code, when you click 'Start Quiz' you will see 'Question' as a header and four buttons labeled 'Answer 1, Answer 2, Answer 3, Answer 4'. User is unable to click an answer, but hover works.
