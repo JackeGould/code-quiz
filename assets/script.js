39cb8d4672e3302f676eb132307209a8e9aa8668
JackeGould <jmgould913@gmail.com>
2024-01-28 22:20:46 -0500

    Final code added

--- assets/script.js
+++ assets/script.js
@@ -87,12 +87,21 @@ var shuffledQuestions, currentQuestionIndex;
 
 // Empty global variables that will be given a value in a local function
 
-// Start button - trigger the first question and next button to display, calling function startGame
-startButton.addEventListener("click", startGame);
-nextButton.addEventListener("click", () => {
-   currentQuestionIndex++
-   setNextQuestion()
-});
+
+
+
+
+// ADD EVENT LISTENERS TO THE BOTTOM OF THE PAGE- HELPS WITH LOADING SPEEED
+// HOISTING IS NOT ALWAYS GOING TO WORK DUE TO SPEED & EFFICIENCY, WRITE FUNCTION BEFORE CALLING
+
+
+
+
+
+
+
+
+
 
 // Timer counts down once game is started, will show count down, if timer runs out, save current score
 function timeTick() {
@@ -143,9 +152,131 @@ function resetState() {
     //clearStatusClass(document.body)
     nextButton.classList.add("hide")
     checkAnswerEl.classList.add("hide")
-    while (answerButtonsEl.firstChild) {
-        answerButtonsEl.removeChild
-            (answerButtonsEl.firstChild)
+answerButtonsEl.textContent= ""
+ };
+ 
+ // Select answer function
+function selectAnswer(e) {
+    var selectedButton = e.target;
+    //console.dir(selectedButton);
+    var correct = selectedButton.dataset.correct;
+    checkAnswerEl.classList.remove("hide")
+    // Check if the answer correct or wrong then show text
+    if (correct) {
+        checkAnswerEl.innerHTML = "You got it right!";
+    } else {
+        checkAnswerEl.innerHTML = "Sorry that was not the correct answer.";
+        if (timeLeft <= 10) {
+            timeLeft = 0;
+        } else {
+            // If the aswer is wrong, deduct time by 10
+            timeLeft -= 10;
+        }
+    }
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
+        checkAnswerEl.classList.remove("hide")
+    } else {
+        startButton.classList.remove("hide")
+        saveScore();
     }
  };
+ // Check and show the correct answer by set the buttons colors
+ function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("wrong");
+    }
+ };
+ // Remove all the classes
+ function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
+ };
+ // Save scores
+ function saveScore() {
+    clearInterval(timerID);
+    timerEl.textContent = "Time: " + timeLeft;
+    setTimeout(function () {
+        //localStorage.setItem("scores", JSON.stringify(scores));
+        questionContainerEl.classList.add("hide");
+        document.getElementById("score-container").classList.remove("hide");
+        document.getElementById("your-score").textContent = "Your final score is " + timeLeft;
+    }, 2000)
+ };
+ var loadScores = function () {
+    // Get score from local storage
+    if (!savedScores) {
+        return false;
+    }
+    // Convert scores from stringfield format into array
+    savedScores = JSON.parse(savedScores);
+    var initials = document.querySelector("#initials-field").value;
+    var newScore = {
+        score: timeLeft,
+        initials: initials
+    }
+    savedScores.push(newScore);
+    console.log(savedScores)
+    savedScores.forEach(score => {
+        initialsField.innerText = score.initials
+        scoreField.innerText = score.score
+    })
+ };
+ // Show high scores
+ function showHighScores(initials) {
+    document.getElementById("highscores").classList.remove("hide")
+    document.getElementById("score-container").classList.add("hide");
+    startContainerEl.classList.add("hide");
+    questionContainerEl.classList.add("hide");
+    if (typeof initials == "string") {
+        var score = {
+            initials, timeLeft
+        }
+        scores.push(score)
+    }
+    var highScoreEl = document.getElementById("highscore");
+    highScoreEl.innerHTML = "";
+    //console.log(scores)
+    for (i = 0; i < scores.length; i++) {
+        var div1 = document.createElement("div");
+        div1.setAttribute("class", "name-div");
+        div1.innerText = scores[i].initials;
+        var div2 = document.createElement("div");
+        div2.setAttribute("class", "score-div");
+        div2.innerText = scores[i].timeLeft;
+        highScoreEl.appendChild(div1);
+        highScoreEl.appendChild(div2);
+    }
+    localStorage.setItem("scores", JSON.stringify(scores));
+ };
+
+ // Start button - trigger the first question and next button to display, calling function startGame
+ startButton.addEventListener("click", startGame);
+ nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+ });
+
+ // View high scores link
+ viewHighScores.addEventListener("click", showHighScores);
+ submitButton.addEventListener("click", function (event) {
+    event.preventDefault()
+    var initials = document.querySelector("#initials-field").value;
+    showHighScores(initials);
+ });
+ // Restart or reload the page
+ restartButton.addEventListener("click", function () {
+    window.location.reload();
+ });
+ // Clear localStorage items
+ clearScoreButton.addEventListener("click", function () {
+    localStorage.clear();
+    document.getElementById("highscore").innerHTML = "";
+ });
  
